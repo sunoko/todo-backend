7d8cc4e762d1e7f9db967449263a35486d3d124a
KazukiChinone <staybesidehangon@gmail.com>
2024-02-20 21:40:43 +0900

    Todo一覧を返すAPI実装

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,9 @@
 class TodosController < ApplicationController
+  # GET /todos
+  def index
+    # 日付が新しい順に10件まで取得する
+    @todos = Todo.all.order(created_at: :desc).limit(10)
+
+    render json: @todos
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Todo.create(title: 'Todo 1', content: 'Todo 1 の内容')
+Todo.create(title: 'Todo 2', content: 'Todo 2 の内容')
+Todo.create(title: 'Todo 3', content: 'Todo 3 の内容')
